0d166244e081a6c49664b98ceceb8e30a2184d48
thiagooo14 <thiagooo15@gmail.com>
2024-07-20 12:40:34 -0300

    feat: create -header-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import Header from './components/Header/Header';
+
+
 function App() {
   return (
-    <>
+    <div>
+      <Header/>
       <h1>Hello world</h1>
-    </>
+    </div>
   )
 }
 
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { logo } from '../../assets'
+import './Header.scss';
+
+const Header: React.FC = () => {
+  return (
+    <header className='header'>
+      <img className='header__logo' src={logo} alt='logo beMobile' />
+    </header>
+  );
+};
+
+export default Header;
